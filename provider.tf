4a1b05cabea8f5031a4e377b41d4fc0834819acb
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-10 19:17:39 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,3 +17,15 @@ provider "azurerm"{
   client_secret=var.client_secret
   tenant_id=var.tenant_id
 }
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
