33c2b9e365d42b7963a735350391d38ebe891e40
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-10 19:45:51 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,20 @@
-
+locals{
+  linux_app=[for f in fileset("${path.module}/linuxappconfiguration", "[^_]*.yaml") : yamldecode(file("${path.module}/linuxappconfiguration/${f}"))]
+  linux_app_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app.listoflinuxapp, []) :{
+        name=linuxapps.name
+        os_type=linuxapps.os_type
+        sku_name=linuxapps.sku_name     
+      }
+    ]
+])
+}
+resource "azurerm_service_plan" "george" {
+  for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
+}
