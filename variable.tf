4f496e75d655a5cc4c61098aaf723bb74687f13c
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-10 20:38:59 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -13,8 +13,8 @@ locals{
 resource "azurerm_service_plan" "george" {
   for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.george_ibrahim.name
+  location            = azurerm_resource_group.george_ibrahim.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
@@ -22,8 +22,8 @@ resource "azurerm_service_plan" "george" {
 resource "azurerm_linux_web_app" "george1980" {
   for_each            = azurerm_service_plan.george
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.george_ibrahim.name
+  location            = azurerm_resource_group.george_ibrahim.location
   service_plan_id     = each.value.id
   enabled             = each.value.enabled
  site_config {}
