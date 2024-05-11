1447989c129f884b09e2943f160bfbc11645af26
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-10 20:39:41 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -25,6 +25,5 @@ resource "azurerm_linux_web_app" "george1980" {
   resource_group_name = azurerm_resource_group.george_ibrahim.name
   location            = azurerm_resource_group.george_ibrahim.location
   service_plan_id     = each.value.id
-  enabled             = each.value.enabled
  site_config {}
 }
