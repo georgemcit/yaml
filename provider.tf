582254acf76e4e1248fb8c0ad69a7d9992b650d2
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-10 20:05:43 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -29,3 +29,35 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "account_tier"{
+  type=string
+  default="Standard"
+}
+variable "access_tier"{
+  type=string
+  default="Cool"
+}
+variable "account_replication_type"{
+  type=string
+  default="GRS"
+}
+variable "cross_tenant_replication_enabled"{
+ type=bool
+ default=false
+}
+variable "environment"{
+ type=string
+ default="staging"
+}
