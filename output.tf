81783e6238ef8c0d7f5e2d2c11c5fc95a92d6a85
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-12 23:38:14 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,9 +4,6 @@ output "azure_linux_app" {
 output "names_linux_app" {
   value=[for app in local.linux_app_list : app.name]
 }
-output "names_linux_app" {
-  value=[for app in local.linux_app_list : app.name]
-}
 output "os_linux_app" {
   value=[for app in local.linux_app_list : app.os_type]
 }
