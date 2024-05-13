88025b068dba8042ec4f2a4b4a53c2ebff3b0d68
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-12 22:56:50 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 locals{
-  linux_app=[for f in fileset("${path.module}/linuxappconfiguration", "[^_]*.yaml") : yamldecode(file("${path.module}/linuxappconfiguration/${f}"))]
+  linux_app=[for f in fileset("${path.module}/${var.linuxappconfiguration}", "[^_]*.yaml") : yamldecode(file("${path.module}/${mar.linuxappconfiguration}/${f}"))]
   linux_app_list = flatten([
     for app in local.linux_app : [
       for linuxapps in try(app.listoflinuxapp, []) :{
