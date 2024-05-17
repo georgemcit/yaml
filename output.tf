2fce519683dccb1ad46aa491dc459de27ba7c75b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-16 22:16:17 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
-output "azure_window_app" {
-  value = local.window_app_list
+output "azure_linux_app" {
+  value = local.linux_app_list
 }
-output "names_window_app" {
-  value=[for app in local.window_app_list : app.name]
+output "names_linux_app" {
+  value=[for app in local.linux_app_list : app.name]
 }
-output "os_window_app" {
-  value=[for app in local.window_app_list : app.os_type]
+output "os_linux_app" {
+  value=[for app in local.linux_app_list : app.os_type]
 }
-output "sku_window_app" {
-  value=[for app in local.window_app_list : app.sku_name]
+output "sku_linux_app" {
+  value=[for app in local.linux_app_list : app.sku_name]
 }
-output "dev" {
-  value = var.vev
+output "linuxappconfiguration" {
+  value = var.linuxappconfiguration
 }
